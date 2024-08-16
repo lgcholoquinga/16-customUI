8bb9be6910204c430976df262707470a968b83b7
lgcholoquinga <luisg.choloquinga@gamil.com>
2024-08-15 22:20:57 -0500

    Learning directives rigth now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HideAfterDirective } from './hide-after.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HideAfterDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
